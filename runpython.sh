74e50d2f24dc71d7d1e78af277d2eba5ff04a0b5
spartacoding-7h-kim <61879561+spartacoding-7h-kim@users.noreply.github.com>
2020-08-18 20:56:12 +0900

    Create runpython.sh

---
+++ runpython.sh
@@ -0,0 +1,8 @@
+# python 이라는 명령어로 3 버전 이상을 실행하도록 하는 명령어입니다.
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+
+# home 디렉토리로 이동
+cd ~
+
+# 실행. 콘솔창에 hellow world!가 뜨는 것을 확인 할 수 있습니다.
+python hello.py
